a5f5eca057756f4928690cbd3810e597be862e11
Matthew Hyrka <mhyrka@gmail.com>
2017-05-04 12:22:18 -0600

    Do It save point. Mostly finished but with errors

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by sally white on 5/4/17.
+//  Copyright © 2017 Matthew Hyrka. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        // Create a Task form the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        // Add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        
+    }
+    
+    
+}
